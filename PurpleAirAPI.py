5e53ea578d563dcf009d80f1ec77bd1610749d05
Carlos <27721404+carlkid1499@users.noreply.github.com>
2022-07-20 21:23:41 -0700

    V0.0.1a
    -- finished get_sensor_data. Am able to fetch data using the method
    -- releasing a very early alpha.

--- PurpleAirAPI.py
+++ PurpleAirAPI.py
@@ -6,9 +6,20 @@
 """
 
 # Python Built In Libs
-from msilib.schema import Error
 import requests
 import json
+
+PRINT_DEBUG_MSGS = True
+
+
+def debug_log(debug_msg_string):
+    """
+        A helper function to print out
+        debug messages only if DEBUG is defined
+    """
+
+    if PRINT_DEBUG_MSGS:
+        print(debug_msg_string)
 
 
 class PurpleAirAPI():
@@ -42,12 +53,12 @@ class PurpleAirAPI():
         if my_request.status_code == 201:
             # We good :) get the request text
             the_request_text_as_json = json.loads(my_request.text)
-            print(the_request_text_as_json)
+            debug_log(the_request_text_as_json)
             self.__api_version = the_request_text_as_json["api_version"]
             self.__api_key_last_checked = the_request_text_as_json["time_stamp"]
             self.__api_key_type = the_request_text_as_json["api_key_type"]
             my_request.close()
-            del my_request 
+            del my_request
             return True
 
         else:
@@ -87,7 +98,8 @@ class PurpleAirAPI():
 
     def get_sensor_data(self, sensor_index, read_key=None, fields=None):
         """
-            A method to retrieve sensor data from one sensor.
+            A method to retrieve sensor data from one sensor. Will return the 
+            response payload as a python dictionary.
 
             :param int sensor_index: The sensor_index as found in the JSON for
                                      this specific sensor.
@@ -109,6 +121,35 @@ class PurpleAirAPI():
                                           https://api.purpleair.com/#api-sensors-get-sensor-data
         """
 
+        request_url = self.__base_api_request_string + \
+            "sensors/" + f"{sensor_index}"
 
+        # Add to the request_url string depending on what optional parameters are
+        # passed in
+        if read_key and fields:
+            request_url = request_url + \
+                f"?read_key={str(read_key)}&fields={str(fields)}"
 
+        elif not read_key and fields:
+            request_url = request_url + f"?fields={str(fields)}"
 
+        elif read_key and not fields:
+            request_url = request_url + f"?read_key={str(read_key)}"
+
+        my_request = requests.get(request_url, headers={
+                                  "X-API-Key": str(self.__your_api_read_key)})
+
+        if my_request.status_code == 200:
+            # We good :) get the request text
+            the_request_text_as_json = json.loads(my_request.text)
+            debug_log(the_request_text_as_json)
+            my_request.close()
+            del my_request
+            return the_request_text_as_json
+
+        elif my_request.status_code == 400:
+            the_request_text_as_json = json.loads(my_request.text)
+            debug_log(the_request_text_as_json)
+            my_request.close()
+            raise ValueError(
+                f"{the_request_text_as_json['error']} - {the_request_text_as_json['description']}")
