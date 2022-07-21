971bd3ce66a97a8112d4cb7df85bae5498d42435
Carlos <27721404+carlkid1499@users.noreply.github.com>
2022-07-20 20:24:40 -0700

    More Methods
    -- added recheck_api_key()
    -- started get_sensor_data()
    -- added returns/raises to __check_an_api_key()

--- PurpleAirAPI.py
+++ PurpleAirAPI.py
@@ -6,8 +6,11 @@
 """
 
 # Python Built In Libs
+from msilib.schema import Error
 import requests
 import json
+
+
 class PurpleAirAPI():
     """
         The PurpleAirAPI class to send only read requests
@@ -28,13 +31,13 @@ class PurpleAirAPI():
 
         self.__check_an_api_key()
 
-
     def __check_an_api_key(self):
         """
             A method to check if an API key is valid.
         """
         request_url = self.__base_api_request_string + "keys"
-        my_request = requests.get(request_url, headers={"X-API-Key": str(self.__your_api_read_key)})
+        my_request = requests.get(request_url, headers={
+                                  "X-API-Key": str(self.__your_api_read_key)})
 
         if my_request.status_code == 201:
             # We good :) get the request text
@@ -43,7 +46,21 @@ class PurpleAirAPI():
             self.__api_version = the_request_text_as_json["api_version"]
             self.__api_key_last_checked = the_request_text_as_json["time_stamp"]
             self.__api_key_type = the_request_text_as_json["api_key_type"]
-    
+            my_request.close()
+            del my_request 
+            return True
+
+        else:
+            raise ValueError(
+                f"Invalid API Key provided: {self.__your_api_read_key}")
+
+    def recheck_api_key(self):
+        """
+            A method to recheck the API Key provided.
+        """
+
+        return self.__check_an_api_key()
+
     @property
     def get_api_version(self):
         """
@@ -55,11 +72,11 @@ class PurpleAirAPI():
     @property
     def get_api_key_last_checked(self):
         """
-            A method to return the API version being used.
+            A method to return the timestamp of when the API Key was last checked.
         """
 
         return self.__api_key_last_checked
-    
+
     @property
     def get_api_key_type(self):
         """
@@ -67,4 +84,31 @@ class PurpleAirAPI():
         """
 
         return self.__api_key_type
+
+    def get_sensor_data(self, sensor_index, read_key=None, fields=None):
+        """
+            A method to retrieve sensor data from one sensor.
+
+            :param int sensor_index: The sensor_index as found in the JSON for
+                                     this specific sensor.
+    
+            :param (optional) str read_key: This read_key is required for
+                                            private devices. It is separate
+                                            to the api_key and each sensor has
+                                            its own read_key. Submit multiple
+                                            keys by separating them with a
+                                            comma (,) character for example:
+                                            key-one,key-two,key-three.
+
+            :param (optional) str fields: The 'Fields' parameter specifies which
+                                          'sensor data fields' to include in the
+                                          response. It is a comma separated list
+                                          with one or more of the following:
+                                          Refer to PurpleAir documentation for more
+                                          information:
+                                          https://api.purpleair.com/#api-sensors-get-sensor-data
+        """
+
+
+
 
