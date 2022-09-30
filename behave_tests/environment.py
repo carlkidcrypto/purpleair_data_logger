a25730c9c9f6876c8478dbb3f033db012263f07c
Carlos <27721404+carlkid1499@users.noreply.github.com>
2022-09-29 21:10:46 -0700

    Behave Test
    -- laying the ground work for the behave tests.

---
+++ behave_tests/environment.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+"""
+    Copyright 2022 carlkid1499, All rights reserved.
+    Behavior driven tests for the purpleair_data_logger.
+"""
+
+
+def before_all(context):
+    """
+        These run before and after the whole shooting match.
+    """
+    pass
+
+
+def after_all(context):
+    """
+        These run before and after the whole shooting match.
+    """
+    pass
+
+
+def before_step(context, step):
+    """
+        These run before and after every step.
+    """
+    pass
+
+
+def after_step(context, step):
+    """
+        These run before and after every step.
+    """
+    pass
+
+
+def before_scenario(context, scenario):
+    """
+        These run before and after each scenario is run.
+    """
+    pass
+
+
+def after_scenario(context, scenario):
+    """
+        These run before and after each scenario is run.
+    """
+    pass
+
+
+def before_feature(context, feature):
+    """
+        These run before and after each feature file is exercised.
+    """
+    pass
+
+
+def after_feature(context, feature):
+    """
+        These run before and after each feature file is exercised.
+    """
+    pass
+
+
+def before_tag(context, tag):
+    """
+        These run before and after a section tagged with the given name.
+        They are invoked for each tag encountered in the order they’re found in the feature file.
+        See controlling things with tags.
+    """
+    pass
+
+
+def after_tag(context, tag):
+    """
+        These run before and after a section tagged with the given name.
+        They are invoked for each tag encountered in the order they’re found in the feature file.
+        See controlling things with tags.
+    """
+    pass
---
+++ behave_tests/steps/steps.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+"""
+    Copyright 2022 carlkid1499, All rights reserved.
+    Steps for behavior driven tests for the purpleair_data_logger.
+"""
