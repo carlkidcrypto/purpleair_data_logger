ec569681c6aaf0b758a479b2e277252ff4514dcc
Carlos <27721404+carlkid1499@users.noreply.github.com>
2022-11-10 09:38:49 -0800

    Nearing EOD
    -- ran one last sphinx build with updated version V1.0.0
    
    -- tweaked the .gitignore file
    -- bumped to version V1.0.0 in setup.py

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.8b1',
+    VERSION: 'V1.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
