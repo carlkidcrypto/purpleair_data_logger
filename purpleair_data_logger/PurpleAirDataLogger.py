1bc5b8a5c8716e2d377f034cd3ad720fa13690ac
Carlos <27721404+carlkid1499@users.noreply.github.com>
2023-02-16 18:05:43 -0800

    Update PurpleAirDataLogger.py
    -- fixing the import

--- purpleair_data_logger/PurpleAirDataLogger.py
+++ purpleair_data_logger/PurpleAirDataLogger.py
@@ -6,7 +6,7 @@
 """
 
 from purpleair_api.PurpleAirAPI import PurpleAirAPI, debug_log
-from purpleair_data_logger.PurpleAirAPIConstants import ACCEPTED_FIELD_NAMES_DICT
+from purpleair_api.PurpleAirAPIConstants import ACCEPTED_FIELD_NAMES_DICT
 from time import sleep
 import json
 
