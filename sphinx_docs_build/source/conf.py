c1148cfac5f562cb6a47bfff130c51cafccaf848
Carlos <27721404+carlkid1499@users.noreply.github.com>
2023-02-25 18:58:32 -0800

    Sphinx Docs
    -- updated shpinx docs
    -- bumped version to V1.2.0

--- sphinx_docs_build/source/conf.py
+++ sphinx_docs_build/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, carlkid1499"
 author = "carlkid1499"
 
 # The full version, including alpha/beta/rc tags
-release = "V1.2.0a2"
+release = "V1.2.0"
 
 
 # -- General configuration ---------------------------------------------------
