690d7b9a5eb75b118341f1e7725db50cf7610c6a
Carlos <dose.lucky.sake@cloak.id>
2023-06-13 21:22:10 -0700

    V1.2.1 (#55)
    - updated docs
    - updated to stable version
    - prepping for official release

--- sphinx_docs_build/source/conf.py
+++ sphinx_docs_build/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, carlkidcrypto"
 author = "carlkidcrypto"
 
 # The full version, including alpha/beta/rc tags
-release = "V1.2.0"
+release = "V1.2.1"
 
 
 # -- General configuration ---------------------------------------------------
