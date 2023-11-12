7ab16d7d640717b1fce79968610bb1550f888c84
Carlos <dose.lucky.sake@cloak.id>
2023-11-12 09:27:39 -0800

    Update test_purpleair_data_logger.py
    - testing as much as possible for PurpleAirDataLogger public interfaces. Next step is move out more logic so the while true loops just call other functions that can be tested individually

--- tests/test_purpleair_data_logger.py
+++ tests/test_purpleair_data_logger.py
@@ -38,3 +38,44 @@ class PurpleAirDataLoggerTest(unittest.TestCase):
         # Expected Result
         self.assertEqual(retval.message, self.error_msg_str)
         self.assertEqual(retval.__class__, PurpleAirDataLoggerError)
+
+    def test_purpleair_data_logger_getters_setters(self):
+        """
+        Test PurpleAirDataLogger getters/setters
+        """
+
+        # Setup
+        fake_url_request = "https://api.purpleair.com/v1/keys"
+
+        # Action and Expected Result
+        with requests_mock.Mocker() as m:
+            m.get(
+                fake_url_request,
+                text='{"api_version" : "1.1.1", "time_stamp": 123456789, "api_key_type": "READ"}',
+                status_code=200,
+            )
+            padl = PurpleAirDataLogger(PurpleAirApiReadKey="123456789")
+
+        padl.send_request_every_x_seconds = 100
+        self.assertEqual(padl.send_request_every_x_seconds, 100)
+
+    def test_purpleair_data_logger_store_sensor_data(self):
+        """
+        Test PurpleAirDataLogger store_sensor_data method raises, since inherting classes
+        must implment it.
+        """
+
+        # Setup
+        fake_url_request = "https://api.purpleair.com/v1/keys"
+
+        # Action and Expected Result
+        with requests_mock.Mocker() as m:
+            m.get(
+                fake_url_request,
+                text='{"api_version" : "1.1.1", "time_stamp": 123456789, "api_key_type": "READ"}',
+                status_code=200,
+            )
+            padl = PurpleAirDataLogger(PurpleAirApiReadKey="123456789")
+
+        with self.assertRaises(NotImplementedError):
+            padl.store_sensor_data({})
