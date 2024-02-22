0af8474c6e80d721aa40be28c09149df225350cc
Carlos <dose.lucky.sake@cloak.id>
2024-02-21 20:04:22 -0800

    Bump
    - ran black
    - updated to v1.3.3
    - update to new 1.1.4 api.

--- sphinx_docs_build/source/conf.py
+++ sphinx_docs_build/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, carlkidcrypto"
 author = "carlkidcrypto"
 
 # The full version, including alpha/beta/rc tags
-release = "V1.3.2"
+release = "V1.3.3"
 
 
 # -- General configuration ---------------------------------------------------
